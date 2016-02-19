ef11c2b665864fd847c75cc8c49851d1b4df793e
Marc Bacvanski <marc.bacvanski@gmail.com>
2016-02-18 21:17:51 -0800

    Graphs linking codes, names, statistics.

--- app/routes.js
+++ app/routes.js
@@ -61,72 +61,77 @@ module.exports = function (app, passport) { //All the routing is handled here
         }));
 
     // Student Stats
-    app.get('/studentStats', isLoggedIn, function (req, res) {
+    app.get('/studentStats/*', isLoggedIn, function (req, res) {
 
+        var url = req.url;
+        var code = url.substr(url.lastIndexOf("/") + 1, url.length);
+        console.log("The code is: " + code);
         var timeFrame = 1; //Days to aggregate
-        var userCode = req.user.code;
-        console.log("userCode: " + userCode);
-        var documentCount = 0;
 
         var Action = require('../app/models/action');
 
-        Action.find({code: userCode}).exec(function (err, results) {
+        Action.find({code: code}).exec(function (err, results) {
             var count = results.length;
-            console.log("Count: " + count);
-            console.log("results: " + results);
+            if (count > 0) {
+                console.log("Count: " + count);
+                console.log("results: " + results);
 
-            console.log();
+                console.log();
 
-            var timeIntervalToSmooth = 2;
+                var timeIntervalToSmooth = 2;
 
-            // Create new empty two dimensional array
-            var data = new Array(count);
-            console.log("Data size: " + data.length);
-            for (var i = 0; i < data.length; i++) {
-                console.log("i = " + i);
-                data[i] = new Array(2);
-            }
-
-            data[0][0] = "Time";
-            data[0][1] = "Number of Users";
-
-            var insertionIndex = 1;
-
-            var currentUsers = 0;
-
-            var firstTime = Date.parse(results[0]._id.getTimestamp());
-            var lastCountedTime = firstTime;
-            for (var i = 0; i < count; i++) {
-                var thisJSON = results[i];
-                console.log("thisJSON: " + thisJSON);
-                if (thisJSON.action === "enterstudy") {
-                    currentUsers++;
-                    console.log("Adding current users!");
-                } else if (thisJSON.action === "exitstudy") {
-                    currentUsers--;
-                    console.log("Subtracting current users!");
+                // Create new empty two dimensional array
+                var data = new Array(count);
+                console.log("Data size: " + data.length);
+                for (var i = 0; i < data.length; i++) {
+                    console.log("i = " + i);
+                    data[i] = new Array(2);
                 }
-                var timeStamp = new Date(thisJSON._id.getTimestamp());
-                console.log("Timestamp: " + timeStamp);
 
-                if (timeStamp - lastCountedTime >= timeIntervalToSmooth) {
-                    lastCountedTime = timeStamp;
-                    var timeString = timeStamp.toString();
-                    data[insertionIndex][0] = timeString;
-                    data[insertionIndex][1] = +currentUsers;
-                    insertionIndex++;
-                    console.log("Wrote into array! insertionIndex = " + insertionIndex);
+                data[0][0] = "Time";
+                data[0][1] = "Number of Users";
+
+                var insertionIndex = 1;
+
+                var currentUsers = 0;
+
+                var firstTime = Date.parse(results[0]._id.getTimestamp());
+                var lastCountedTime = firstTime;
+                for (var i = 0; i < count; i++) {
+                    var thisJSON = results[i];
+                    console.log("thisJSON: " + thisJSON);
+                    if (thisJSON.action === "enterstudy") {
+                        currentUsers++;
+                        console.log("Adding current users!");
+                    } else if (thisJSON.action === "exitstudy") {
+                        currentUsers--;
+                        console.log("Subtracting current users!");
+                    }
+                    var timeStamp = new Date(thisJSON._id.getTimestamp());
+                    console.log("Timestamp: " + timeStamp);
+
+                    if (timeStamp - lastCountedTime >= timeIntervalToSmooth) {
+                        lastCountedTime = timeStamp;
+                        var timeString = timeStamp.toString();
+                        data[insertionIndex][0] = timeString;
+                        data[insertionIndex][1] = +currentUsers;
+                        insertionIndex++;
+                        console.log("Wrote into array! insertionIndex = " + insertionIndex);
+                    }
                 }
-            }
 
-            console.log("Here comes the data: -------------------------------");
-            for (var i = 0; i < data.length; i++) {
-                console.log(data[i]);
-            }
+                console.log("Here comes the data: -------------------------------");
+                for (var i = 0; i < data.length; i++) {
+                    console.log(data[i]);
+                }
 
-            res.render('graphs.ejs', {
-                graphData: data
-            })
+                res.render('studentStats.ejs', {
+                    graphData: data
+                })
+            } else {
+                var profilePath = "../../profile/";
+                res.render('nodata.ejs', {profilePath: profilePath});
+            }
         });
     });
 };
