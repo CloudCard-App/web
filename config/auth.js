541e8aae6047046e8f1c51912c6df420341aa8ed
Marc Bacvanski <marc.bacvanski@gmail.com>
2016-05-19 20:30:00 -0700

    Fixed OAuth callback URL

--- config/auth.js
+++ config/auth.js
@@ -3,6 +3,6 @@ module.exports = {
     'googleAuth': { //Can be found right after you create new credentials.
         'clientID': '282384112298-iaq8n3j02r73hjkcut2cat26n9kbmeii.apps.googleusercontent.com',
         'clientSecret': '1WnJVe7GV8Mb4gV_6QYZzPWx',
-        'callbackURL': 'http://localhost:8080/auth/google/callback' //Should match that defined in the Google credentials!
+        'callbackURL': 'http://cloudcard.us/auth/google/callback' //Should match that defined in the Google credentials!
     }
 };
