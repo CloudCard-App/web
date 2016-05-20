4a7e547b5f7f424e3c29bb52090317790438b55a
Marc Bacvanski <marc.bacvanski@gmail.com>
2016-05-19 20:32:36 -0700

    Fixed Google Oauth client ID and secret

--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,8 @@
 module.exports = {
     // These will not work if they don't match the ones defined in Google API
     'googleAuth': { //Can be found right after you create new credentials.
-        'clientID': '282384112298-iaq8n3j02r73hjkcut2cat26n9kbmeii.apps.googleusercontent.com',
-        'clientSecret': '1WnJVe7GV8Mb4gV_6QYZzPWx',
+        'clientID': '932314628179-coftdulrbjgo9m61hkd163eaetr6qt61.apps.googleusercontent.com',
+        'clientSecret': 'JvxyV1-RB0PPKaIhEj9c2xQS',
         'callbackURL': 'http://cloudcard.us/auth/google/callback' //Should match that defined in the Google credentials!
     }
 };
