1aee8f834989071907e35546fc9c0b220a3c071b
Marc Bacvanski <marc.bacvanski@gmail.com>
2016-06-06 22:17:39 -0700

    Add sample tests

---
+++ tests/test.js
@@ -0,0 +1,9 @@
+var assert = require('chai').assert;
+
+describe("Testing for funz", function () {
+  describe("More funz", function () {
+    it("tests for equality", function () {
+      assert.equal(2, 2);
+    });
+  });
+});
